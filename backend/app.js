98c86f150f5cf4b8cca488e3b44159a23a366e10
anushkasomani <somanianu956@gmail.com>
2024-08-08 16:28:01 +0530

    Your commit message

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors=require('cors');
 const app = express();
+
+app.use(cors());
 
 const URL = "mongodb+srv://anushka:anushkas@cluster0.w2aa386.mongodb.net/sample_geospatial?retryWrites=true&w=majority&appName=Cluster0";
 
@@ -12,6 +15,9 @@ mongoose.connect(URL)
         console.error("Oops!! Some error occurred: ", err);
     });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+
+
+
+app.listen(3009, () => {
+    console.log('Server is running on port 3009');
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Updates from './components/Updates';
 import MainPage from './components/MainPage';
 import Profile from './components/Profile';
+import axios from 'axios';
+import { response } from 'express';
+
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [dataResponse, setDataResponse] = useState(null);
+
+
+    useEffect(()=>{
+      axios.get('http://localhost:3009/')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(console.error()
+      )
+    },[]);
+
   return (
     <Router>
       <Routes>
