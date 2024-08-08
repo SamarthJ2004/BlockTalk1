5e7dac4bf7dfba5abeeba1ad9c375d333f49d786
Samarth Jindal <samarth.jindal2004@gmail.com>
2024-08-08 14:08:12 +0530

    resolved refesh

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Init from './Init';
 import { SEPOLIA_ID } from '../config';
-import MainPage from './MainPage';
 
 function Home() {
   const [currentAccount, setCurrentAccount] = useState('');
